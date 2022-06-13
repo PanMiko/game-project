cc5fcb3e89dd40b69f913f3d4d8b43e80c824e6c
piterm33 <piterm33@gmail.com>
2022-06-13 17:44:59 +0200

    added timer

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,10 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public Transform PlayerTransform;
+
+    public TextMeshProUGUI text;
+    private float timer;
+
+    void Awake()
+    {
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        var minutes = (int)timer / 60;
+        var seconds = (int)timer % 60;
+        if (seconds < 10)
+        {
+            var textSeconds = $"0{seconds}";
+            text.text = $"{minutes}:{textSeconds}";
+        }
+        else
+        {
+            var textSeconds = seconds.ToString();
+            text.text = $"{minutes}:{textSeconds}";
+        }
+        
+    }
 
     void LateUpdate()
     {
